fadde3ce960c7e65d1b0957520d7b1d6b5c0d947
Lucy Lee <gamesandhannah@gmail.com>
2022-01-11 16:56:32 -0500

    Create Search component that takes user input for city

---
+++ city-search/src/index.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+// let city = this.input.value
+// city.toUpperCase()
+
+class Search extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {value: ''};
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(event) {
+        this.setState({value: event.target.value});
+    }
+
+    handleSubmit(event) {
+        alert('A city was submitted: ' + this.state.value);
+        event.preventDefault();
+    }
+
+    render() {
+        return (
+            <form onSubmit={this.handleSubmit}>
+                <label>
+                    City:
+                    <input type="text" value={this.state.value} onChange={this.handleChange} />
+                </label>
+                <input type="submit" value="Submit" />
+            </form>
+        );
+    }
+}
+
+// ========================================
+
+ReactDOM.render(
+    <Search />,
+    document.getElementById('root')
+);
